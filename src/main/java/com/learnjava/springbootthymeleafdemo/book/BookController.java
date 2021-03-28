bdc88d886bf87708021e2ba3f3f75d919df0529d
Reshma Bidikar <50294888+learnjavawithreshma@users.noreply.github.com>
2021-03-28 17:15:54 +0530

    Initial Commit

---
+++ src/main/java/com/learnjava/springbootthymeleafdemo/book/BookController.java
@@ -0,0 +1,29 @@
+package com.learnjava.springbootthymeleafdemo.book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@Value("${greeting}")
+	private String greeting;
+	
+	 @GetMapping("/books")
+	  public String getAllBooks(Model model){
+	    //Returns hardcoded data, a real world application would return from the database
+	    List<Book> books = new ArrayList<Book>();
+	    books.add(new Book(1,"Emma", "Jane Austen"));
+	    books.add(new Book(2,"Harry Potter", "JK Rowling"));
+	    books.add(new Book(3,"The Partner", "John Grisham"));
+	    model.addAttribute("books", books);
+	    model.addAttribute("greeting",greeting);
+	    return "books";
+	  } 
+
+}
